e4a9899506c21bb19245e817bebd61cc87fd62be
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-03-14 12:17:43 +1100

    Update ClearDB.php

--- content/ClearDB.php
+++ content/ClearDB.php
@@ -7,6 +7,8 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
-echo $conn;
-echo "A testing message";
+echo $server;
+echo $username;
+echo $password;
+echo $db;
 ?>
