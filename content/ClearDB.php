748237fdc1154146c667cbb4d55762cda05163e4
henryliu18 <45472005+henryliu18@users.noreply.github.com>
2019-03-14 12:38:23 +1100

    Update ClearDB.php

--- content/ClearDB.php
+++ content/ClearDB.php
@@ -7,8 +7,58 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
-echo "Server:".$server."<br>";
-echo "Username:".$username."<br>";
-echo "Password:".$password."<br>";
-echo "db:".$db."<br>";
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+// sql to create table
+$sql = "CREATE TABLE MyGuests (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+// sql to insert data to table
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// sql to select data
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+// sql to drop table
+$sql = "DROP TABLE MyGuests";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table MyGuests dropped successfully";
+} else {
+    echo "Error dropping table: " . $conn->error;
+}
+
+$conn->close();
 ?>
